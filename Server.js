cb2e8f2b91108fddd499d4c21ea0a06c42758cda
Ashutosh Mishra <121817779+ashugh12@users.noreply.github.com>
2024-05-24 22:39:37 +0530

    Update Server.js with cross-origin issues

--- Server.js
+++ Server.js
@@ -5,10 +5,10 @@ const routes = require('./routes/ToDoRoutes')
 const cors= require("cors")
 const app = express();
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your frontend origin
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "https://your-netlify-app.netlify.app"); // Replace with your frontend origin
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const PORT = process.env.port || 5000;
 app.use(express.json());
 app.use(routes);
